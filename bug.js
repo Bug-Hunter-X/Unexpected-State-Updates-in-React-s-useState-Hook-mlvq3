9d3af0e3ce205b4f03dcd3736eefcf7e1d552bb7
BugHunter-X <askzenai@gmail.com>
2025-02-15 15:48:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,22 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This effect runs after every render
+    console.log('Count:', count);
+  }, [count]); 
+
+  const handleClick = () => {
+    setCount(count + 1);
+    setCount(count + 1); //This line causes the issue
+  };
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+}
+```
